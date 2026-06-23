6230caf393fbfd0cba9b9b7905dd7fd9456aed0f
mithun00gowda <mithun00gowda@gmail.com>
2026-06-23 08:46:06 +0530

    added auth repository implementation

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -2,12 +2,12 @@ import 'package:blog_app/core/error/failures.dart';
 import 'package:fpdart/fpdart.dart';
 
 abstract interface class AuthRepository {
-  Either<Failures, String> signUpWithEmailPassword({
+  Future<Either<Failures, String>> signUpWithEmailPassword({
    required String name,
    required String email,
    required String password
 });
-  Either<Failures, String> logInWithEmailPassword({
+  Future<Either<Failures, String>> logInWithEmailPassword({
    required String email,
    required String password
 });
